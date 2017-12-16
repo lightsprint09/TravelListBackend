7f91688246c48740501bdeb7df6b706f2cc34744
Terenze Yuen <terenze.yuen@digitalworkroom.co.uk>
2017-12-16 09:06:43 +0100

    lock file

--- provider/airbnb.js
+++ provider/airbnb.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 var WAE = require('web-auto-extractor').default
 
 class Extractor {
-	
+
 	extractFrom(url, created) {
 		return fetch(url)
 		.then(function(res) {
@@ -16,11 +16,11 @@ class Extractor {
 			let metatags = WAE().parse(body).metatags
 			console.log(metatags)
 			let accommodation = new Accommodation(url, metatags.name, metatags.description, null, "accomodation", created);
-			
+
 		})
 		.then(function(result) {
 			console.log(result)
-		}) 
+		})
 /*
 		return new Promise(function(resolve, reject) {
 			osmosis
@@ -31,33 +31,33 @@ class Extractor {
 				let json = JSON.parse(jsonText.data)
 				let location = parseLocationFrom(json.hasMap);
 				let accommodation = new Accommodation(url, json.name, json.description, location, "accomodation", created);
-				 
+
 		    	resolve(accommodation)
 			})
 		})
 */
-		
+
 	}
 }
 
 function parseLocationFrom(url) {
 	const myURL = new URL(url);
 	let coordinates = myURL.searchParams.get('center').split(",")
-	return new Location(coordinates[0] * 1, coordinates[1] * 1, true);
+	return new Location(coordinates[0] * 1, coordinates[1] * 1, false);
 }
 
 function testData(date) {
-	return{ 
+	return{
 		webUrl: 'https://www.booking.com/hotel/it/the-smallest-hostel-of-florence.de.html',
 		name: 'The Smallest Hostel of Florence',
 		descriptionText: 'Das The Smallest Hostel of Florence begrüßt Sie in Florenz, nur 5 Gehminuten von der Basilika Santa Maria Novella entfernt.',
 		category: { id: "accomodation" },
 		created: date,
-		location:  { 
+		location:  {
 			latitude: 43.7753645,
 			longitude: 11.2545613,
-			exactLocation: true 
-		} 
+			exactLocation: true
+		}
     }
 }
 
